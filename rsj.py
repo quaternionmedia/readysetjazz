9b12ed121bd96bcba7208ea9abefa2d1d70c4dd5
Harpo <harpo@harpo.me>
2017-05-24 12:18:29 -0700

    functional backend with sample photos and videos and random ordering

--- rsj.py
+++ rsj.py
@@ -1,28 +1,48 @@
 import hug
-from os import walk
+from os import walk, path
 from jinja2 import Environment, FileSystemLoader
+from PIL import Image
+from random import shuffle
 env = Environment(loader=FileSystemLoader('templates'))
 
-html = hug.get(output=hug.output_format.html)
+#api = hug.get(output=hug.output_format.html, on_invalid=hug.redirect.not_found, )
+
+@hug.startup()
+def thumbPics(api): # Check pics folder, make thumbs
+	thumbs = []
+	for dirpath, dirname, filenames in walk('thumbs'): #list all thumbs
+		for t in filenames:
+			if t.endswith('.jpg'):
+				thumbs.append(t)
+
+	for dirpath, dirname, filenames in walk('pics'):
+		for p in filenames:
+			if p.endswith('.jpg') and p not in thumbs: # convert it
+				im = Image.open(path.join(dirpath, p))
+				im.thumbnail((100, 100), Image.ANTIALIAS)
+				im.save(path.join('thumbs', p))
+#	return
 
 @hug.local()
 @hug.get()
 def songlist():
-	songs = {}
+	songs = []
 	limit = 10
 	for dirpath, dirname, filenames in walk('audio'):
 		for f in filenames:
 			if f.endswith('.mp3') and len(songs) <= limit:
-				songs[f] = 'audio/%s' % f
+				songs.append(f)
+	shuffle(songs)
 	return songs
 
 def piclist():
-	pics = {}
+	pics = []
 	limit = 10
 	for dirpath, dirname, filenames in walk('pics'):
 		for f in filenames:
 			if f.endswith('.jpg') and len(pics) <= limit:
-				pics[f] = 'audio/%s' % f
+				pics.append(f)
+	shuffle(pics)
 	return pics
 
 #@html
@@ -40,20 +60,20 @@ def home():
 def pics():
 	return('pics',)
 @hug.static('/audio')
-def pics():
+def audio():
 	return('audio',)
 @hug.static('/thumbs')
-def pics():
+def thumbs():
 	return('thumbs',)
 @hug.static('/css')
-def pics():
+def css():
 	return('css',)
 @hug.static('/js')
-def pics():
+def js():
 	return('js',)
 @hug.static('/images')
-def pics():
+def images():
 	return('images',)
 @hug.static('/img')
-def pics():
+def img():
 	return('img',)
